d8446a99388d71cded86f03ff9ed15a18dca7c64
Itay Banner <itay@ibanner.co.il>
2020-02-13 00:33:12 +0200

    added sounds when buttons are clicked

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-$(".simon-button-container").click(function () {
+$(".simon-button").click(function() {
   var $this = $(this);
+  var currentColor = this.id;
+  console.log(currentColor);
   $this.addClass("pressed");
+  playSound(currentColor);
   setTimeout(function() {
     $this.removeClass("pressed");
   }, 100);
 });
+
+
+// Sound Effects
+var sounds = {
+  green: new Audio("sounds/simonSoundGreen.mp3"),
+  red: new Audio("sounds/simonSoundRed.mp3"),
+  cyan: new Audio("sounds/simonSoundCyan.mp3"),
+  yellow: new Audio("sounds/simonSoundYellow.mp3"),
+}
+
+function playSound(currentColor) {
+  sounds[currentColor].play();
+}
